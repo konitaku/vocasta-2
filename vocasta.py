b9da5ae06acb11e23705f3279be76bf638659f27
Takuma Konishi <takm2357msc@icloud.com>
2021-07-23 22:32:44 +0900

    Add SQLALCHEMY_TRACK_MODIFICATIONS and some modification

--- vocasta.py
+++ vocasta.py
@@ -10,8 +10,9 @@ from functools import wraps
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///vocasta.db")
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 Bootstrap(app)
 login_manager = LoginManager(app)
